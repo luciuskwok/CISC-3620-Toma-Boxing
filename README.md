0007e06ae76ca3346d54a275f466d8cac8b24192
Lucius Kwok <lk@felttip.com>
2024-03-24 15:43:42 -0400

    SDL2 random pixel test

--- README.md
+++ README.md
@@ -4,5 +4,13 @@
 - Download and install Emscripten from <https://emscripten.org/docs/getting_started/downloads.html>
 - `cd` into the emsdk folder
 - `source ./emsdk_env.sh` to add the paths
-- Basic build: `emcc main.c -o main.html`
-- Custom HTML: `emcc -o main.html main.c -O3 --shell-file _templates/shell_minimal.html`
+
+## Building "Hello world"
+- Basic build: `emcc main.c -o index.html`
+- Custom HTML: `emcc main.c -o index.html -O3 --shell-file _templates/shell_minimal.html`
+- Build JS without changing HTML: `emcc main.c -o index.js -O3`
+
+## Building with SDL2
+- `emcc -c main.c -o main.o -s USE_SDL=2`
+- `emcc main.o -o main.js -s USE_SDL=2`
+- `emcc main.c -o main.js -s USE_SDL=2`
--- main.c
+++ main.c
@@ -1,8 +1,42 @@
 // main.c
 
-#include <stdio.h>
+#include <SDL.h>
+#include <emscripten.h>
+#include <stdlib.h>
 
-int main() {
-	printf("My name is Toma, and I'm a safe driver!\n");
-	return 0;
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_Surface *surface;
+
+#define PIXEL_W (640)
+#define PIXEL_H (400)
+
+
+void drawRandomPixels() {
+    if (SDL_MUSTLOCK(surface)) SDL_LockSurface(surface);
+
+    Uint8 * pixels = surface->pixels;
+    
+    for (int i=0; i < PIXEL_W * PIXEL_H * 4; i++) {
+        char randomByte = rand() % 255;
+        pixels[i] = randomByte;
+    }
+
+    if (SDL_MUSTLOCK(surface)) SDL_UnlockSurface(surface);
+
+    SDL_Texture *screenTexture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, screenTexture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+
+    SDL_DestroyTexture(screenTexture);
+}
+
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(PIXEL_W, PIXEL_H, 0, &window, &renderer);
+    surface = SDL_CreateRGBSurface(0, PIXEL_W, PIXEL_H, 32, 0, 0, 0, 0);
+    
+    emscripten_set_main_loop(drawRandomPixels, 0, 1);
 }
